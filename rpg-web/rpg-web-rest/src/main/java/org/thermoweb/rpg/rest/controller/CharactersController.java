c3c1cacb60b4f04f3afd5e296130745d5469378a
thermo <G@lifred63>
2023-01-23 22:56:24 +0100

    add starter equipment linked to the profile. add random spell at character creation (based on level). remove equipment from CharacterCreationRequest.

--- rpg-web/rpg-web-rest/src/main/java/org/thermoweb/rpg/rest/controller/CharactersController.java
+++ rpg-web/rpg-web-rest/src/main/java/org/thermoweb/rpg/rest/controller/CharactersController.java
@@ -6,21 +6,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thermoweb.core.utils.RandomUtils;
 import org.thermoweb.rpg.actions.Spells;
 import org.thermoweb.rpg.characters.Ability;
 import org.thermoweb.rpg.characters.CharacterService;
 import org.thermoweb.rpg.characters.Skills;
 import org.thermoweb.rpg.characters.Statistics;
 import org.thermoweb.rpg.dto.CharacterDto;
+import org.thermoweb.rpg.equipment.Equipment;
 import org.thermoweb.rpg.equipment.TorsoArmor;
 import org.thermoweb.rpg.equipment.Weapon;
 import org.thermoweb.rpg.rest.client.CharacterCreationRequest;
 import org.thermoweb.rpg.rest.mapper.CharacterMapper;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/characters")
@@ -81,9 +85,14 @@ public class CharactersController {
                 .maxHitPoints(20)
                 .statistics(Map.of(Ability.FORCE, startStats.force(), Ability.DEXTERITY, startStats.dexterity(), Ability.INTELLIGENCE, startStats.intelligence()))
                 .skills(skills)
-                .spellbook(skills.contains(Skills.SPELL_CASTING) ? List.of(Spells.FIREBALL, Spells.FROSTBOLT) : Collections.emptyList())
-                .equipment(request.equipment())
+                .spellbook(skills.contains(Skills.SPELL_CASTING) ? getRandomSpells() : Collections.emptyList())
+                .equipment(request.profile().getStarterSet().stream().collect(Collectors.toMap(Equipment::getSlot,
+                        e -> CharacterDto.Equipment.builder().name(e.getName()).attrition(100).build())))
                 .build();
         return CharacterMapper.map(characterService.create(CharacterMapper.map(characterDto)));
+    }
+
+    private static List<Spells> getRandomSpells() {
+        return Collections.singletonList(RandomUtils.getRandomItem(Arrays.stream(Spells.values()).filter(spells -> spells.getLevel() == 1).toList()));
     }
 }
