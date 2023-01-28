6e183faca65e3e10f21b84a59fba6970ee463ed1
thermo <G@lifred63>
2023-01-28 02:01:44 +0100

    add encounter creation form in the character page

--- rpg-web/rpg-web-front/src/main/java/org/thermoweb/rpg/front/views/EncountersView.java
+++ rpg-web/rpg-web-front/src/main/java/org/thermoweb/rpg/front/views/EncountersView.java
@@ -4,11 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thermoweb.rpg.dto.EncounterDto;
+import org.thermoweb.rpg.dto.brain.BrainDto;
 import org.thermoweb.rpg.encounter.client.EncounterManagerClient;
 import org.thermoweb.rpg.encounter.client.EncounterManagerRestClient;
+import org.thermoweb.rpg.rest.client.EncounterCreationRequest;
+import org.thermoweb.rpg.rest.client.RpgArenaClient;
+import org.thermoweb.rpg.rest.client.RpgArenaRestClient;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,6 +25,7 @@ import java.util.List;
 public class EncountersView {
 
     private final EncounterManagerClient encounterManagerRestClient = new EncounterManagerRestClient("http", "localhost", "8082");
+    private final RpgArenaClient rpgArenaClient = new RpgArenaRestClient("http", "localhost", "8080");
     ;
 
     @GetMapping
@@ -30,6 +37,22 @@ public class EncountersView {
             throw new RuntimeException(e);
         }
         return "encounters_list";
+    }
+
+    @PostMapping("create")
+    public String createEncounter(@ModelAttribute CreateEncounterForm request) {
+        try {
+            EncounterDto encounter = rpgArenaClient.createRandomEncounter(EncounterCreationRequest.builder()
+                    .characterId(request.characterId())
+                    .opponentId(request.opponentId())
+                    .brain(new BrainDto(request.brainType(), request.brainUri()))
+                    .grid(request.grid())
+                    .build());
+
+            return "redirect:/ui/encounters/" + encounter.id();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @GetMapping("{id}")
