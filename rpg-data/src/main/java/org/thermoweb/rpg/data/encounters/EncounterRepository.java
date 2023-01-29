7a34563f2f1deebe601554b9528f7d1dc0f7c20d
thermo <romain.chauveau+github@protonmail.com>
2023-01-29 16:18:20 +0100

    improve frontend

--- rpg-data/src/main/java/org/thermoweb/rpg/data/encounters/EncounterRepository.java
+++ rpg-data/src/main/java/org/thermoweb/rpg/data/encounters/EncounterRepository.java
@@ -1,11 +1,16 @@
 package org.thermoweb.rpg.data.encounters;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.thermoweb.rpg.encounters.EncounterStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EncounterRepository extends MongoRepository<EncounterEntity, String> {
 
     Optional<EncounterEntity> findFirstByStatus(EncounterStatus status);
+
+    @Query("{ 'characters._id' : ?0 }")
+    List<EncounterEntity> findAllByCharacterId(String id);
 }
